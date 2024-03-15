cade8bb619a3d18f522d64e66179c48ca59d97d7
Ginevra Testa <ginevra99testaa@gmail.com>
2024-03-15 20:24:35 +0100

    adjustments

--- backend/models/authors.js
+++ backend/models/authors.js
@@ -4,22 +4,27 @@ const AuthorSchema = new mongoose.Schema(
   {
     name: {
       type: String,
+      required: true
     },
 
     surname: {
       type: String,
+      required: true
     },
 
     email: {
       type: String,
+      required: true
     },
 
     birthday: {
       type: String,
+      required: true
     },
 
     avatar: {
       type: String,
+      required: true
     },
   },
   { timestamps: true, strict: true }
