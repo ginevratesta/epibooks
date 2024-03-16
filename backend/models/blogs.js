7324d8752c90db2b912668490db49b011929db71
Ginevra Testa <ginevra99testaa@gmail.com>
2024-03-16 16:26:10 +0100

    get post patch delete + model for blogs

---
+++ backend/models/blogs.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const BlogsSchema = new mongoose.Schema(
+  {
+    category: {
+      type: String,
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+
+    cover: {
+      type: String,
+      required: true,
+    },
+
+    readTime: {
+      type: Number,
+      required: true,
+    },
+
+    author: {
+      type: String,
+      required: true,
+    },
+
+    content: {
+      type: String,
+      required: true
+    },
+  },
+  { timestamps: true, strict: true }
+);
+
+module.exports = mongoose.model("blogModel", BlogsSchema, "blogs");
---
+++ backend/routes/blogs.js
@@ -0,0 +1,118 @@
+const express = require("express");
+const router = express.Router();
+const blogModel = require("../models/blogs");
+
+router.get("/getblogs", async (req, res) => {
+  try {
+    const blogs = await blogModel.find();
+    res.status(200).send(blogs);
+  } catch (e) {
+    res.status(500).send({
+      statusCode: 500,
+      message: "Internal Server Error",
+    });
+  }
+});
+
+router.post("/createBlog", async (req, res) => {
+  const newBlog = new blogModel({
+    name: req.body.name,
+    surname: req.body.surname,
+    email: req.body.email,
+    birthday: req.body.birthday,
+    avatar: req.body.avatar,
+  });
+
+  try {
+    const blogToSave = await newBlog.save();
+    res.status(201).send({
+      statusCode: 201,
+      payload: blogToSave,
+    });
+  } catch (e) {
+    res.status(500).send({
+      statusCode: 500,
+      message: "Internal Server Error",
+    });
+  }
+});
+
+
+router.get("/getBlog/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const blog = await blogModel.findById(id);
+
+    if (!blog) {
+      return res.status(404).send({
+        statusCode: 404,
+        message: "The request blog does not exist!",
+      });
+    }
+    res.status(200).send(blog);
+  } catch (error) {
+    res.status(500).send({
+      statusCode: 500,
+      message: "Internal Server Error",
+    });
+  }
+});
+
+router.patch("/updateBlog/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const blog = await blogModel.findById(id);
+
+    if (!blog) {
+      return res.status(404).send({
+        statusCode: 404,
+        message: "The request blog does not exist!",
+      });
+    }
+
+    const updatedData = req.body;
+    const options = { new: true };
+
+    const results = await blogModel.findByIdAndUpdate(
+      id,
+      updatedData,
+      options
+    );
+
+    res.status(200).send(results);
+  } catch (error) {
+    res.status(500).send({
+      statusCode: 500,
+      message: "Internal Server Error",
+    });
+  }
+});
+
+
+router.delete("/deleteBlog/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const blog = await blogModel.findByIdAndDelete(id);
+
+    if (!blog) {
+      return res.status(404).send({
+        statusCode: 404,
+        message: 'The request blog does not exist',
+      });
+    }
+
+    res.status(200).send('The blog has been removed');
+  } catch (error) {
+    res.status(500).send({
+      statusCode: 500,
+      message: "Internal Server Error",
+    });
+  }
+});
+
+
+
+
+module.exports = router;
