2809d5a935377202a656c9dce41e958c84eff001
Ginevra Testa <ginevra99testaa@gmail.com>
2024-03-26 15:17:51 +0100

    adjustments

--- frontend/src/Components/footer/Footer.jsx
+++ frontend/src/Components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css"
 const Footer = () => {
     return(
         <footer className="d-flex align-items-center justify-content-center">
-            <p>Contact us at:  333 333 3333</p>
+            <p>Contact us:  333 333 3333</p>
         </footer>
     )
 }
