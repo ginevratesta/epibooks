05fb4c41d01ed20faf9e4dc454513960df385bf2
Ginevra Testa <ginevra99testaa@gmail.com>
2024-04-06 14:03:56 +0200

    login and sign up  completed

--- frontend/src/Components/login/Login.jsx
+++ frontend/src/Components/login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Container, Form, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
@@ -9,6 +9,7 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const navigate = useNavigate();
+  const [isRegistered, setIsRegistered] = useState(true);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -33,15 +34,43 @@ const Login = () => {
     }
   };
 
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    setEmail("");
+    setPassword("");
+
+    try {
+      console.log(email, password);
+      const response = await axios.post("http://localhost:3030/signup", {
+        email,
+        password,
+      });
+      console.log(response.data);
+    } catch (error) {
+      setError("Error registering user");
+      console.error("Error registering user:", error);
+    } finally {
+      setEmail("");
+      setPassword("");
+      navigate("/home");
+    }
+  };
+
   const handleGitHubLogin = () => {
     window.location.href = `${process.env.REACT_APP_SERVER_BASE_URL}/auth/github`;
   };
 
+  const handleSignUp = () => {
+    setIsRegistered(false);
+  };
+
+  useEffect(() => {}, [isRegistered, email, password]);
+
   return (
     <Container className="py-5 text-center">
-      <h2>Login</h2>
+      <h2>{isRegistered ? "Login" : "Sign up"}</h2>
       <Form
-        onSubmit={handleSubmit}
+        onSubmit={isRegistered ? handleSubmit : handleRegister}
         className="d-flex flex-column justify-content-around align-items-center"
       >
         <Form.Group controlId="email">
@@ -64,22 +93,23 @@ const Login = () => {
         </Form.Group>
         {error && <p className="text-danger">{error}</p>}
         <Button variant="primary" type="submit" className="mt-3">
-          Login
+         {isRegistered ? "Login" : "Sign up"}
         </Button>
-      </Form>
+        <button
+          className="github w-25 my-5"
+          type="button"
+          onClick={handleGitHubLogin}
+        >
+          <img
+            id="github-img"
+            src="https://tse3.mm.bing.net/th?id=OIP.Sfgbqcg35rCru0YB-IQwxgHaD4&pid=Api&P=0&h=180"
+            alt="github logo"
+          />
+          Login with GitHub
+        </button>
 
-      <button
-        className="github w-25 my-5"
-        type="button"
-        onClick={handleGitHubLogin}
-      >
-        <img
-          id="github-img"
-          src="https://tse3.mm.bing.net/th?id=OIP.Sfgbqcg35rCru0YB-IQwxgHaD4&pid=Api&P=0&h=180"
-          alt="github logo"
-        />
-        Login with GitHub
-      </button>
+        <p onClick={handleSignUp} style={{ cursor: "pointer" }}>Not a member yet? Sign up!</p>
+      </Form>
     </Container>
   );
 };
